d95ea0e434771e3886fb3618e89c54d656ac1de9
Diego Rubin <contact@diegorubin.dev>
2020-11-19 17:13:10 -0300

    example 01: adding action

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export LIFEGUARD_DIRECTORY="."
-export LIFEGUARD_VALIDATION_REPOSITORY_IMPLEMENTATION="lifeguard_mongodb.repositories.MongoDBValidationRepository"
+export LIFEGUARD_VALIDATION_REPOSITORY_IMPLEMENTATION="lifeguard_mongodb.MongoDBValidationRepository"
 
-lifeguard-scheduler
+lifeguard
--- validations/pudim_validation.py
+++ validations/pudim_validation.py
@@ -4,11 +4,16 @@ Check if pudim is alive
 import requests
 
 from lifeguard import NORMAL, PROBLEM, change_status
+from lifeguard.actions.database import save_result_into_database
 from lifeguard.logger import lifeguard_logger as logger
 from lifeguard.validations import validation, ValidationResponse
 
 
-@validation("check if pudim is alive", actions=[], schedule={"every": {"minutes": 1}})
+@validation(
+    "check if pudim is alive",
+    actions=[save_result_into_database],
+    schedule={"every": {"minutes": 1}},
+)
 def pudim_is_alive():
     status = NORMAL
     result = requests.get("http://pudim.com.br")
