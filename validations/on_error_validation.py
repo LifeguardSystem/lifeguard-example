2195168e55dccc1bd4dfb52f908079106959a3fb
Diego Rubin <contact@diegorubin.dev>
2023-06-06 16:45:31 -0300

    feat: adding example of openai action

--- validations/on_error_validation.py
+++ validations/on_error_validation.py
@@ -1,11 +1,14 @@
-from lifeguard.validations import validation, ValidationResponse
 from lifeguard.actions.notifications import notify_in_thread
+from lifeguard.http_client import post
+from lifeguard.validations import validation
+
+from lifeguard_openai.actions.errors import explain_error
 
 
 @validation(
     "This validation will ever return a error. The objective is to test the actions on error",
     actions=[notify_in_thread],
-    actions_on_error=[notify_in_thread],
+    actions_on_error=[explain_error, notify_in_thread],
     schedule={"every": {"minutes": 1}},
     settings={
         "notification": {
@@ -18,9 +21,14 @@ from lifeguard.actions.notifications import notify_in_thread
 ```
 {{traceback}}
 ```
+
+*explanation*
+```
+{{explanation}}
+```
 """,
         }
     },
 )
-def validate_on_error():
-    raise Exception("error")
+def on_error_example():
+    a_not_imported_function()
